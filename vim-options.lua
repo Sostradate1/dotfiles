b060253080f4588b7215bfabbcfab8550fd3ae44
Your Name <you@example.com>
2024-03-20 21:11:09 +0300

    bind only

--- vim-options.lua
+++ vim-options.lua
@@ -4,8 +4,12 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
-
 -- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
 vim.keymap.set('n', '<c-Up>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-Down>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-Left>', ':wincmd h<CR>')
@@ -13,9 +17,8 @@ vim.keymap.set('n', '<c-Right>', ':wincmd l<CR>')
 vim.keymap.set('n', '<Enter>', ':set paste<CR>m`o<Esc>``:set nopaste<CR>' )
 
 
-vim.keymap.set("n", "<S-c>", ":tabnew<CR>", opts)
-vim.keymap.set("n", "<S-x>", "gT", opts)
-
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
+vim.api.nvim_set_option("clipboard","unnamedplus")
+
 
